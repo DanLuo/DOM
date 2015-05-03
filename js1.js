bb63c9607c32d89b6d5ae671ed886ab064d5ea62
DanLuo <dluo1019@gmail.com>
2015-05-03 01:27:02 -0400

    add file

---
+++ js1.js
@@ -0,0 +1,5 @@
+//var a = prompt("give new head");
+newhead=function()
+{
+var name="hello";
+document.getElementById("p1").innerHTML = name;};
---
+++ js2.js
@@ -0,0 +1,5 @@
+var head=document.getElementById("main");
+var newhead=prompt("new head");
+//document.getElementById("p1").innerHTML="Hello world";
+//head.innerHTML=newhead;
+$("#main").text(newhead);
