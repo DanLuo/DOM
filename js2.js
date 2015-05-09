ca5fec8ad75f446d79af1d656b3052ab2fca2a3f
DanLuo <dluo1019@gmail.com>
2015-05-08 20:23:14 -0400

    modif

--- js2.js
+++ js2.js
@@ -1,5 +1,12 @@
-var head=document.getElementById("main");
+var head=document.getElementById("p1");
 var newhead=prompt("new head");
 //document.getElementById("p1").innerHTML="Hello world";
 //head.innerHTML=newhead;
-$("#main").text(newhead);
+$("#p1").text(newhead);
+
+$("h1").fadeOut(3000).fadeIn(2000);
+
+for (var i=0; i<3;i++)
+{var hobby=prompt("Tell me your hobby");
+ $("body").append("<p>"+hobby+"</p>");}
+
